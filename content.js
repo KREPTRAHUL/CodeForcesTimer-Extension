da7160f96c9b0c16e86dca87e761993a38da3ef4
Sanmaireddy <ysreddy377@gmail.com>
2023-07-29 14:15:58 +0530

    added timer

--- content.js
+++ content.js
@@ -1,3 +1,44 @@
+const maxTime = 300;
+let intervalId;
+
+const getId = () => {
+  const currentURL = window.location.toString();
+  const len = currentURL.length;
+  let ct = 0,
+    id = "";
+  for (let i = len - 1; i >= 0; i--) {
+    if (currentURL.charAt(i) == "/") {
+      if (ct == 1) break;
+      ct++;
+    }
+    id += currentURL.charAt(i);
+  }
+  console.log(id);
+  id = id.split("").reverse().join("");
+  return id;
+};
+
+const startTimer = () => {
+  const currentTime = new Date();
+  const startTime = currentTime.getTime();
+
+  const id = getId();
+  localStorage.setItem(id, startTime);
+
+  timeLeft = maxTime;
+  let intervalId = setInterval(function () {
+    timeLeft--;
+  });
+  const val = localStorage.getItem(id);
+  console.log(val);
+};
+
+const resetTimer = () => {
+  const id = getId();
+  localStorage.removeItem(id);
+  //add the timer
+};
+
 const addContent = () => {
   const timer = document.createElement("div");
   timer.id = "timer";
@@ -7,12 +48,16 @@ const addContent = () => {
 
   const mybuttons = document.createElement("div");
   mybuttons.id = "mybuttons";
+
   const start = document.createElement("button");
   start.textContent = "Start";
   start.id = "start";
+  start.addEventListener("click", startTimer);
+
   const reset = document.createElement("button");
   reset.textContent = "Reset";
   reset.id = "reset";
+  reset.addEventListener("click", resetTimer);
 
   mybuttons.appendChild(start);
   mybuttons.appendChild(reset);
---
+++ practice.js
@@ -0,0 +1,5 @@
+const currentDate = new Date();
+const timestamp = currentDate.getTime();
+
+console.log("Current Date:", currentDate);
+console.log("Timestamp in milliseconds:", timestamp);
