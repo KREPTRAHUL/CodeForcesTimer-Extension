25a0a0d387500a1effdb5f4d7947e44825fc056c
Sanmaireddy <ysreddy377@gmail.com>
2023-08-07 09:18:57 +0530

    Timer Functionality Complete for Contest Problems

--- content.js
+++ content.js
@@ -24,17 +24,31 @@ const getId = () => {
   //Id of the question
   const currentURL = window.location.toString();
   const len = currentURL.length;
-  let ct = 0,
+  if (currentURL.includes("contest")) {
     id = "";
-  for (let i = len - 1; i >= 0; i--) {
-    if (currentURL.charAt(i) == "/") {
-      if (ct == 1) break;
-      ct++;
+    for (let i = 31; i < len; i++) {
+      if (currentURL.charAt(i) == "/") {
+        id += "/";
+        id += currentURL.charAt(i + 9);
+        break;
+      }
+      id += currentURL.charAt(i);
     }
-    id += currentURL.charAt(i);
+    console.log(id);
+    return id;
+  } else {
+    let id = "";
+    for (let i = 42; i < len; i++) {
+      if (currentURL.charAt(i) == "/") {
+        id += "/";
+        id += currentURL.charAt(i + 1);
+        break;
+      }
+      id += currentURL.charAt(i);
+    }
+    console.log(id);
+    return id;
   }
-  id = id.split("").reverse().join("");
-  return id;
 };
 
 const updateTimerDisplay = (timeLeft) => {
@@ -42,25 +56,35 @@ const updateTimerDisplay = (timeLeft) => {
   timeElem.textContent = getFormattedTime(timeLeft);
 };
 
-const addSubmitTime = () => {
+const getSubmissionsCount = () => {
+  const tableRows = document.querySelector(".rtable.smaller tbody");
+  if (!tableRows) return 0;
+  const count = tableRows.querySelectorAll("tr").length - 1;
+  return count;
+};
+
+const addSubmitTimeAndSubmissionsCount = () => {
   const currentTime = new Date();
   const currTime = currentTime.getTime() / 1000;
   const id = getId();
   const attributesInStorage = JSON.parse(localStorage.getItem(id));
   attributesInStorage.submission_Time = currTime;
+  attributesInStorage.submissoins_Count = getSubmissionsCount();
   localStorage.setItem(id, JSON.stringify(attributesInStorage));
 };
 const activateSubmitButtonandStore = () => {
   const submiBtn = document.getElementById("sidebarSubmitButton");
-  submiBtn.addEventListener("click", addSubmitTime);
+  submiBtn.addEventListener("click", addSubmitTimeAndSubmissionsCount);
 };
 
 const deactivateSubmitButton = () => {
   const submiBtn = document.getElementById("sidebarSubmitButton");
-  submiBtn.removeEventListener("click", addSubmitTime);
+  submiBtn.removeEventListener("click", addSubmitTimeAndSubmissionsCount);
 };
 
 const startTimer = (time) => {
+  const timeElem = document.getElementById("timeElem");
+  if (timeElem.textContent.includes("Time")) return;
   if (intervalId) return;
 
   const currentTime = new Date();
@@ -101,6 +125,7 @@ const resetTimer = () => {
   localStorage.removeItem(id);
   const timeElem = document.getElementById("timeElem");
   timeElem.textContent = getFormattedTime(defaultMaxTime);
+  timeElem.style.fontSize = "25px";
   deactivateSubmitButton();
 };
 
@@ -162,6 +187,7 @@ const checkIfTimerRunning = (attributes) => {
 };
 
 const getRecentVerdict = () => {
+  //returns recent verdict
   const tableRows = document.querySelector(".rtable.smaller tbody");
   const recentSubmission = tableRows
     .querySelector("tr:nth-child(2) td:nth-child(3)")
@@ -175,11 +201,26 @@ const recoverFromLocalStorage = () => {
 
   let id = getId();
   let attributes = localStorage.getItem(id);
+  console.log(attributes);
 
-  if (!attributes) return;
+  if (!attributes) {
+    const alreadySolved = localStorage.getItem(`TimeTaken_${id}`);
+    console.log(alreadySolved);
+    if (alreadySolved) {
+      const timeElem = document.getElementById("timeElem");
+      timeElem.textContent = "Time Taken:";
+      timeElem.textContent += getFormattedTime(alreadySolved);
+      timeElem.style.fontSize = "25px";
+    }
+    return;
+  }
   attributes = JSON.parse(attributes);
-  if (attributes.submission_Time) {
+  if (
+    attributes.submission_Time &&
+    attributes.submissoins_Count + 1 == getSubmissionsCount()
+  ) {
     const verdict = getRecentVerdict();
+    console.log("verdit", verdict);
     if (verdict) {
       const timeElem = document.getElementById("timeElem");
       timeElem.textContent = "Time Taken:";
@@ -192,20 +233,46 @@ const recoverFromLocalStorage = () => {
       );
       localStorage.removeItem(id);
       return;
+    } else {
+      delete attributes["submission_Time"];
+      delete attributes["submissoins_Count"];
+      localStorage.setItem(id, JSON.stringify(attributes));
     }
   }
   const isTimerRunning = checkIfTimerRunning(attributes); //take cares of Time Out also
-  if (!isTimerRunning) return;
+  if (!isTimerRunning) {
+    return;
+  }
 
   const date = new Date();
   const currTime = date.getTime() / 1000;
   startTimer(attributes.max_Time - (currTime - attributes.start_Time));
 };
 
+const checkIfContestRunning = () => {
+  const sideTableLength = document
+    .querySelector(".rtable tbody")
+    .querySelectorAll("tr").length;
+  if (sideTableLength == 5) return true;
+  return false;
+};
+
 const insertTimer = () => {
   window.addEventListener("load", () => {
-    addContent();
-    recoverFromLocalStorage();
+    const currentURL = window.location.toString(); //different URL for contest ques and problemset ques, So needed to handeled.
+    if (currentURL.includes("contest")) {
+      if (currentURL.includes("problem")) {
+        const checkContestRunning = checkIfContestRunning(); //checking if there is a contest running
+        if (!checkContestRunning) {
+          addContent();
+          recoverFromLocalStorage();
+        }
+      }
+    } else {
+      addContent();
+      recoverFromLocalStorage();
+      console.log(getSubmissionsCount());
+    }
   });
 };
 
